a2e2b180932784b21276ebfd9ba8566b73fed705
lenxyang <lenxyang@gmail.com>
2015-12-24 10:18:18 +0800

    .

--- parallax_occlusion_mapping/effect.h
+++ parallax_occlusion_mapping/effect.h
@@ -94,5 +94,40 @@ class MaterialProvider : public Material {
 };
 
 typedef scoped_refptr<MyEffect> MyEffectPtr;
+
+using azer::Effect;
+using azer::EffectParamsProvider;
+using azer::EffectAdapterKey;
+using azer::EffectParamsAdapter;
+
+class MaterialEffectAdapter : public EffectParamsAdapter {
+ public:
+  MaterialEffectAdapter();
+
+  EffectAdapterKey key() const override;
+  void Apply(Effect* e, const EffectParamsProvider* params) const override;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MaterialEffectAdapter);
+};
+
+class SceneRenderNodeEffectAdapter : public EffectParamsAdapter {
+ public:
+  SceneRenderNodeEffectAdapter();
+
+  EffectAdapterKey key() const override;
+  void Apply(Effect* e, const EffectParamsProvider* params) const override;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SceneRenderNodeEffectAdapter);
+};
+
+class SceneRenderEnvNodeEffectAdapter : public EffectParamsAdapter {
+ public:
+  SceneRenderEnvNodeEffectAdapter();
+
+  EffectAdapterKey key() const override;
+  void Apply(Effect* e, const EffectParamsProvider* params) const override;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SceneRenderEnvNodeEffectAdapter);
+};
 }  // namespace sandbox
 }  // namespace lord
