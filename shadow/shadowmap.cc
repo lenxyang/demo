bdfbe0f2722fb5fbe6797a607ca9b574217d4cee
lenxyang <lenxyang@gmail.com>
2016-01-05 09:53:02 +0800

    add renderer switch

--- shadow/shadowmap.cc
+++ shadow/shadowmap.cc
@@ -12,16 +12,60 @@ using namespace lord;
 
 class MyRenderWindow : public lord::FrameWindow {
  public:
-  MyRenderWindow(const gfx::Rect& rect) : lord::FrameWindow(rect) {}
+  MyRenderWindow(const gfx::Rect& rect) 
+      : lord::FrameWindow(rect),
+        default_renderer_(false) {
+  }
+
   SceneNodePtr InitScene() override;
   void OnUpdateFrame(const azer::FrameArgs& args) override;
   void OnRenderFrame(const azer::FrameArgs& args, Renderer* renderer) override;
+  void switch_renderer() { default_renderer_ = !default_renderer_;}
  private:
   SceneRenderNodePtr bvolumn_root_;
-  scoped_ptr<EffectedSceneRenderer> tree_render_;
+  scoped_ptr<EffectedSceneRenderer> effected_render_;
+  scoped_ptr<UISceneRenderer> scene_render_;
   scoped_ptr<ShadowDepthRenderer> depth_render_;
   EffectDict dict_;
+  bool default_renderer_;
+  bool switching_;
   DISALLOW_COPY_AND_ASSIGN(MyRenderWindow);
+};
+
+class RendererToolbar : public nelf::Toolbar,
+                        public views::ButtonListener {
+ public:
+  RendererToolbar(MyRenderWindow* render_window)
+      : nelf::Toolbar(render_window),
+        render_window_(render_window) {
+    LordEnv* context = LordEnv::instance();
+    int32 toolbar_id = IDR_ICON_TOOLBAR_LAYERS;
+
+    using views::BoxLayout;
+    views::View* contents = new views::View;
+    contents->SetLayoutManager(new BoxLayout(BoxLayout::kHorizontal, 0, 1, 0));
+    nelf::ResourceBundle* bundle = context->resource_bundle();
+    int32 id = toolbar_id;
+    const gfx::ImageSkia* img = bundle->GetImageSkiaNamed(id);
+    button_ = new nelf::ToggleButton(*img);
+    button_->SetInsets(gfx::Insets(1, 1, 1, 1));
+    button_->SetImageLabelSpacing(0);
+    button_->set_listener(this);
+    button_->set_tag(id);
+    button_->SetMinSize(gfx::Size(32, 32));
+    button_->SetTooltipText(::base::UTF8ToUTF16("This is tooltip"));
+    contents->AddChildView(button_);
+    SetContents(contents);
+  }
+  ~RendererToolbar() {
+  }
+ private:
+  void ButtonPressed(views::Button* sender, const ui::Event& event) override {
+    render_window_->switch_renderer();
+  }
+  MyRenderWindow* render_window_;
+  nelf::ToggleButton* button_;
+  DISALLOW_COPY_AND_ASSIGN(RendererToolbar);
 };
 
 int main(int argc, char* argv[]) {
@@ -44,6 +88,10 @@ int main(int argc, char* argv[]) {
   window->SetShowIcon(true);
   window->Init();
   window->Show();
+
+  RendererToolbar* toolbar = new RendererToolbar(window);
+  toolbar->Float();
+  toolbar->Dock(0, 1);
   window->GetRenderLoop()->Run();
   return 0;
 }
@@ -61,16 +109,27 @@ SceneNodePtr MyRenderWindow::InitScene() {
   SceneNodePtr root = res.scene;
   CHECK(root.get()) << "Failed to init scene";
 
-  tree_render_.reset(new EffectedSceneRenderer);
-  tree_render_->Init(root, &camera());
+  effected_render_.reset(new EffectedSceneRenderer);
+  effected_render_->Init(root, &camera());
+
+  scene_render_.reset(new UISceneRenderer);
+  scene_render_->Init(root, &camera());
 
   return root;
 }
 
 void MyRenderWindow::OnUpdateFrame(const FrameArgs& args) {
-  tree_render_->Update(args);
+  if (default_renderer_) {
+    scene_render_->Update(args);
+  } else {
+    effected_render_->Update(args);
+  }
 }
 
 void MyRenderWindow::OnRenderFrame(const FrameArgs& args, Renderer* renderer) {
-  tree_render_->Render(renderer);
+  if (default_renderer_) {
+    scene_render_->Render(renderer);
+  } else {
+    effected_render_->Render(renderer);
+  }
 }
